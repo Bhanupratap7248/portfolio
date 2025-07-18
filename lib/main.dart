43f5d9da66f24d7dd74351d8ac606fe39f966e6c
Bhanu pratap <142300187+Bhanupratap7248@users.noreply.github.com>
2025-07-18 22:03:08 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'PORTFOLIO',
+      theme: ThemeData(
+        // This is the theme of your application.
+        badgeTheme: const BadgeThemeData(
+          backgroundColor: Colors.greenAccent,
+          textStyle: TextStyle(color: Colors.white),
+        ),
+        primarySwatch: Colors.deepPurple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('Portfolio')),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Center(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(200.0),
+                  child: Image.asset(
+                    'assets/images/bhanu.png',
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 2),
+            Text(
+              'BHANU PRATAP',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 5),
+            Text("Study in  GLA university,"),
+            Divider(color: Colors.black, height: 20, thickness: 2),
+            
+          ],
+        ),
+        
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
