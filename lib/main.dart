b9179e22be779e35ac2ac5902fa62037d29b66e0
Bhanu pratap <142300187+Bhanupratap7248@users.noreply.github.com>
2025-07-21 12:24:51 +0530

    change

--- lib/main.dart
+++ lib/main.dart
@@ -66,95 +66,102 @@ class MyApp extends StatelessWidget {
                   ],
                 ),
               ),
-              SizedBox(height: 5),
 
               Divider(color: Colors.blueAccent, thickness: 3),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(20.0),
-                      child: Text(
-                        'EDUCATION : ',
-                        textAlign: TextAlign.right,
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.blueAccent,
-                        ),
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 10.0),
+                    child: Text(
+                      'EDUCATION : ',
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueAccent,
                       ),
                     ),
-                    SizedBox(height: 10),
-                    Row(
-                      children: [
-                        Column(
-                          children: [
-                            Text(
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.normal,
-                                color: Colors.black,
-                              ),
-                              "Bachelor of Technology in Computer Science, GLA University, Mathura        June 2026"
-                              "\n"
-                              "   Diploma  GLA University, Mathura                                                                    June 2020"
-                              "\n"
-                              "Intermediate from Uttar Predesh Board     							                                                  May 2017"
-                              "\n"
-                              "High School from Uttar Predesh Board     						                                                     May 2015",
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              Divider(color: Colors.blueAccent, thickness: 1),
               SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Padding(
-                      padding: EdgeInsets.all(20.0),
-                      child: Text(
-                        'INTERNSHIP/TRAINING EXPERIENCE :',
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.blueAccent,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          "GLA University Mathura,",
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(20.0),
-
-                          child: Text(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
                             textAlign: TextAlign.left,
                             style: TextStyle(
                               fontSize: 15,
                               fontWeight: FontWeight.normal,
                               color: Colors.black,
                             ),
-                            "JOVAC  Python Machine learning & Data Science                                                                        June 2024 – July 2024"
+                            "Bachelor of Technology in Computer Science, GLA University, Mathura        June 2026"
                             "\n"
-                            "•	Automating loan approvals using machine learning for accurate risk assessment."
+                            "Diploma  GLA University, Mathura                                                                    June 2020"
                             "\n"
-                            "•	Predicting loan eligibility based on applicant credit history, income, and financial behavior.",
+                            "Intermediate from Uttar Predesh Board     							                                                  May 2017"
+                            "\n"
+                            "High School from Uttar Predesh Board     						                                                     May 2015",
                           ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Divider(color: Colors.blueAccent, thickness: 1),
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 10.0),
+                    child: Text(
+                      'INTERNSHIP/TRAINING EXPERIENCE :',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10.0),
+                      child: Text(
+                        "GLA University Mathura,",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.black,
+                          ),
+                          "   JOVAC  Python Machine learning & Data Science                                                                                                       June 2024 – July 2024"
+                          "\n"
+                          "             •	Automating loan approvals using machine learning for accurate risk assessment."
+                          "\n"
+                          "             •	Predicting loan eligibility based on applicant credit history, income, and financial behavior.",
                         ),
                       ],
                     ),
@@ -165,7 +172,7 @@ class MyApp extends StatelessWidget {
               Row(
                 children: [
                   Padding(
-                    padding: EdgeInsets.all(20.0),
+                    padding: EdgeInsets.all(10.0),
                     child: Text(
                       'PROJECTS :',
                       style: TextStyle(
@@ -178,11 +185,14 @@ class MyApp extends StatelessWidget {
                 ],
               ),
               Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.only(left: 2.0),
+                    padding: const EdgeInsets.only(left: 10.0),
                     child: Text(
-                      "Loan Approval  Predictions System",
+                      "Loan Approval  Predictions System"
+                      "\n"
+                      "Sep 2024 – Nov 2024",
                       textAlign: TextAlign.left,
                       style: TextStyle(
                         fontSize: 15,
@@ -192,19 +202,26 @@ class MyApp extends StatelessWidget {
                     ),
                   ),
                   Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Text(
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.normal,
-                        color: Colors.black,
+                    padding: EdgeInsets.all(10.0),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          Text(
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.normal,
+                              color: Colors.black,
+                            ),
+                            "•	 A mechanism called the Loan Prediction System allows you to apply for loans and receive notifications when they are approved."
+                            "\n"
+                            "     By the data provided by the applicant, the system notifies the applicant of the loan's availability."
+                            "\n"
+                            "•	Created using Python, Pandas, NumPy, Matplotlib, Seaborn, Scikit-Learn.",
+                          ),
+                        ],
                       ),
-                      "•	 A mechanism called the Loan Prediction System allows you to apply for loans and receive notifications when they are approved."
-                      "\n"
-                      " By the data provided by the applicant, the system notifies the applicant of the loan's availability."
-                      "\n"
-                      "•	Created using Python, Pandas, NumPy, Matplotlib, Seaborn, Scikit-Learn.",
                     ),
                   ),
                 ],
@@ -226,7 +243,78 @@ class MyApp extends StatelessWidget {
                   ),
                 ],
               ),
-              Column(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 20.0),
+                        child: Text(
+                          "Technical Skills: ",
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10.0),
+                        child: Text(
+                          "Flutter App Development"
+                          "\n"
+                          "JAVA Programming"
+                          "\n"
+                          "DBMS,Operating system"
+                          "\n"
+                          "Computer Networks",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 20.0),
+                        child: Text(
+                          "ProfessionalS Skills: ",
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10.0),
+                        child: Text(
+                          "Effective Communication Skills"
+                          "\n"
+                          "Ability to identify and solve problems"
+                          "\n"
+                          "Teamwork and Collaboration",
+
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
               Divider(color: Colors.blueAccent, thickness: 1),
               Row(
                 children: [
