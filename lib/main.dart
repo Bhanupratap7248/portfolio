fba47cbf9b196628b850d323b6f56ee92148f847
Bhanu pratap <142300187+Bhanupratap7248@users.noreply.github.com>
2025-07-21 11:04:45 +0530

    change

--- lib/main.dart
+++ lib/main.dart
@@ -28,111 +28,239 @@ class MyApp extends StatelessWidget {
           title: Center(child: Text('Portfolio')),
           backgroundColor: Colors.deepPurple,
         ),
-        body: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Center(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(200.0),
-                  child: Image.asset(
-                    'assets/images/bhanu.png',
-                    width: 100,
-                    height: 100,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 2),
-            Text(
-              'BHANU PRATAP',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic,
-                color: Colors.black,
-              ),
-            ),
-            SizedBox(height: 5),
-            Text("Study in  GLA university,"),
-            Divider(color: Colors.black, thickness: 1),
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Text(
-                    'Education : ',
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Center(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(200.0),
+                    child: Image.asset(
+                      'assets/images/bhanu.png',
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.cover,
                     ),
                   ),
                 ),
-                SizedBox(height: 10),
-                Column(
+              ),
+              SizedBox(height: 2),
+              Center(
+                child: Column(
                   children: [
                     Text(
-                      "Bachelor of Technology in Computer Science, GLA University, Mathura                       	June 2026",
-                    ),
-                    Text(
-                      "   Diploma  GLA University, Mathura                                                                                                                                  June 2020",
+                      'BHANU PRATAP'
+                      '\n'
+                      "Hathaura ● Baldeo, 281301"
+                      '\n'
+                      "bhanu.pratap_cs23@gla.ac.in  ●+917248415131",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.blueAccent,
+                      ),
                     ),
                   ],
                 ),
-              ],
-            ),
-            Divider(color: Colors.black, thickness: 1),
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Text(
-                    'Education :',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
+              ),
+              SizedBox(height: 5),
+
+              Divider(color: Colors.blueAccent, thickness: 3),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Text(
+                        'EDUCATION : ',
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blueAccent,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.black,
+                              ),
+                              "Bachelor of Technology in Computer Science, GLA University, Mathura        June 2026"
+                              "\n"
+                              "   Diploma  GLA University, Mathura                                                                    June 2020"
+                              "\n"
+                              "Intermediate from Uttar Predesh Board     							                                                  May 2017"
+                              "\n"
+                              "High School from Uttar Predesh Board     						                                                     May 2015",
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Divider(color: Colors.blueAccent, thickness: 1),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Text(
+                        'INTERNSHIP/TRAINING EXPERIENCE :',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blueAccent,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "GLA University Mathura,",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(20.0),
+
+                          child: Text(
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.normal,
+                              color: Colors.black,
+                            ),
+                            "JOVAC  Python Machine learning & Data Science                                                                        June 2024 – July 2024"
+                            "\n"
+                            "•	Automating loan approvals using machine learning for accurate risk assessment."
+                            "\n"
+                            "•	Predicting loan eligibility based on applicant credit history, income, and financial behavior.",
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Divider(color: Colors.blueAccent, thickness: 1),
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text(
+                      'PROJECTS :',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueAccent,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Divider(color: Colors.black, thickness: 1),
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Text(
-                    'Education :',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
+                ],
+              ),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 2.0),
+                    child: Text(
+                      "Loan Approval  Predictions System",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Divider(color: Colors.black, thickness: 1),
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Text(
-                    'Education :',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
+                  Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text(
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black,
+                      ),
+                      "•	 A mechanism called the Loan Prediction System allows you to apply for loans and receive notifications when they are approved."
+                      "\n"
+                      " By the data provided by the applicant, the system notifies the applicant of the loan's availability."
+                      "\n"
+                      "•	Created using Python, Pandas, NumPy, Matplotlib, Seaborn, Scikit-Learn.",
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+
+              Divider(color: Colors.blueAccent, thickness: 1),
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text(
+                      'SKILLS:',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Column(),
+              Divider(color: Colors.blueAccent, thickness: 1),
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text(
+                      'EXTRA/CO-CURRICULAR ACTIVITIES	:',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Divider(color: Colors.blueAccent, thickness: 1),
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text(
+                      'DECLARATION:',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
 
         backgroundColor: Colors.white,
